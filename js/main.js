96820fd0dfca3ce28a02de6dc58761c49294832f
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-02-01 10:46:35 +0700

    offsetWidth

--- js/main.js
+++ js/main.js
@@ -20,6 +20,12 @@ var photos = [
 var xValues = [];
 var yValues = [];
 
+var mapBlock = document.querySelector('.map').offsetWidth;
+var iconHeight = 70;
+var iconWidth = 50;
+var locationMinY = 130;
+var locationMaxY = 630;
+
 var getRandomValues = function (count) {
   for (var i = 1; i <= count; i++) {
     var valueAddres = String(Math.round(200 - 0.5 + Math.random() * (650 - 200 + 1)));
@@ -27,12 +33,6 @@ var getRandomValues = function (count) {
 
     var price = Math.floor(Math.random() * 5000) * 1200;
     prices.push(price);
-
-    var valuesX = Math.round(100 - 0.5 + Math.random() * (800 - 100 + 1));
-    xValues.push(valuesX);
-
-    var valuesY = Math.round(130 - 0.5 + Math.random() * (630 - 130 + 1));
-    yValues.push(valuesY);
 
     var avatar = 'img/avatars/user0' + i + '.png';
     avatars.push(avatar);
@@ -42,7 +42,14 @@ var getRandomValues = function (count) {
 
     var guest = Math.round(1 - 0.5 + Math.random() * 12);
     guestsNumbers.push(guest);
+
+    xValues.push(getRandomPosition(iconWidth, mapBlock));
+    yValues.push(getRandomPosition(locationMinY, locationMaxY));
   }
+};
+
+var getRandomPosition = function (min, max) {
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
 
 getRandomValues(objCount);
@@ -116,8 +123,8 @@ var renderPin = function (arg) {
   for (var i = 0; i < arrayOfObjects.length; i++) {
     var pinElement = pinTemplate.cloneNode(true);
 
-    pinElement.style.left = (arg[i].location.x - 25) + 'px';
-    pinElement.style.top = (arg[i].location.y - 70) + 'px';
+    pinElement.style.left = (arg[i].location.x - iconWidth / 2) + 'px';
+    pinElement.style.top = (arg[i].location.y - iconHeight) + 'px';
 
     pinElement.querySelector('img').src = arg[i].author.avatar;
     pinElement.querySelector('img').alt = arg[i].offer.title;
